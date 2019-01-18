57fd535f54089a57b76eabf1d92f85483702c352
Tam Le <tle@Tams-MacBook-Pro-2.local>
2019-01-18 14:00:13 -0800

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:baby_eagle/baby_eagle.dart' as baby_eagle;
+
+main(List<String> arguments) {
+  print('Hello world: ${baby_eagle.calculate()}!');
+}
---
+++ test/baby_eagle_test.dart
@@ -0,0 +1,8 @@
+import 'package:baby_eagle/baby_eagle.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
